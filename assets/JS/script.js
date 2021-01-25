10671a6c980eb5a2bbd41cc8d7e2d2eafb3758c7
ymasog <yer.masog@gmail.com>
2021-01-24 22:18:51 -0600

    cleaned up code one last time!

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -79,26 +79,19 @@ function gameEnd() {
 }
 
 function saveHighscore() {
-    // get value of input box
      var name = nameEl.value.trim();
-  
-    // make sure value wasn't empty
+
     if (name !== "") {
-      // get saved scores from localstorage, or if not any, set to empty array
       var highscores =
         JSON.parse(window.localStorage.getItem("highscores")) || [];
   
-      // format new score object for current user
       var newScore = {
         score: scoreCounter,
         name: name
       };
-  
-      // save to localstorage
       highscores.push(newScore);
       window.localStorage.setItem("highscores", JSON.stringify(highscores));
-  
-      // redirect to next page
+
       window.location.href = "score.html";
     }
   }
